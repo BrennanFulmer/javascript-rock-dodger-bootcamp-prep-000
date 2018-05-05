4db49dc71d10b338c24edd4f42f43a695e2058da
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-05 08:00:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -108,8 +108,9 @@ function moveDodgerRight() {
   const west = parseInt(dodger.style.left.replace(`px`, ``));
 
   function move() {
+    DODGER.style.left = `${west + 20}px`;
+    
     if (west < 360) {
-      DODGER.style.left = `${west + 20}px`;
       window.requestAnimationFrame(move);
     }
   }
