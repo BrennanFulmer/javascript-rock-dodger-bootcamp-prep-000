7e3baa3aaabe968cc149cd8a3fc24d688aed2e7e
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-05 07:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -95,15 +95,10 @@ function moveDodgerLeft() {
   const left = parseInt(dodger.style.left.replace(`px`, ``));
 
   function step() {
-<<<<<<< HEAD
     window.requestAnimationFrame(step);
 
-=======
-    DODGER.style.left = `${left - 4}px`;
-    
->>>>>>> 1623fae1f085a035e8f93932ba1b4e70965a3a65
     if (left > 0) {
-      window.requestAnimationFrame(step);
+      DODGER.style.left = `${left - 4}px`;
     }
   }
   window.requestAnimationFrame(step);
@@ -113,15 +108,10 @@ function moveDodgerRight() {
   const west = parseInt(dodger.style.left.replace(`px`, ``));
 
   function move() {
-<<<<<<< HEAD
     window.requestAnimationFrame(move);
 
-=======
-    DODGER.style.left = `${west + 4}px`;
-    
->>>>>>> 1623fae1f085a035e8f93932ba1b4e70965a3a65
     if (west < 360) {
-      window.requestAnimationFrame(move);
+      DODGER.style.left = `${west + 4}px`;
     }
   }
   window.requestAnimationFrame(move);
